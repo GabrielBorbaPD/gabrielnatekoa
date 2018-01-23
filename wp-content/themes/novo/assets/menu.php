3d49a362e07e5313194bc222ad5d890e66c0c0a9
GabrielBorbaPD <35711748+GabrielBorbaPD@users.noreply.github.com>
2018-01-22 23:35:45 -0200

    Update menu.php

--- wp-content/themes/novo/assets/menu.php
+++ wp-content/themes/novo/assets/menu.php
@@ -1,6 +1,6 @@
 <div class="container">
   <div class="menu-area">
-    <img src="<?php bloginfo('template_url'); ?>/images/logotipo-tekoa.png">
+    <a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('template_url'); ?>/images/logotipo-tekoa.png"></a>
     <div class="top-menu text-right"><?php wp_nav_menu( array('theme-location' => 'top_menu')); ?></div>
   </div>
 </div>
