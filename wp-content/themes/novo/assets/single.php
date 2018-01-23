96b3cd5b72cc54b0328d8768b599a8ecb75649dd
GabrielBorbaPD <35711748+GabrielBorbaPD@users.noreply.github.com>
2018-01-22 23:27:53 -0200

    Update single.php

--- wp-content/themes/novo/assets/single.php
+++ wp-content/themes/novo/assets/single.php
@@ -2,8 +2,6 @@
 
 <div class="content">
 	<div class="container">
-		<div class="breadcrumb"><?php wp_custom_breadcrumbs(); ?></div>
-		<div class="clear"></div>
 		<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
 			<?php the_post_thumbnail( 'full') ?>
 			<?php the_content(); ?>
